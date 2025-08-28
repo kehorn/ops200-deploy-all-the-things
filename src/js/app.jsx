c8ca23a4ed3f5552add25a3e466eaa06fd6cb2df
Kevin H <kehorn@yahoo.com>
2025-08-28 14:59:00 -0700

    Changes text from Heroku to Vercel.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <h1>My First Heroku App</h1>
+      <div className="App">
+        <h1>My First Vercel App</h1>
       </div>
     );
   }
